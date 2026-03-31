8d62b1625f57f93c2c0a04d7acdf448b444d642c
ruhul2003 <ruhul941020@gmail.com>
2026-04-01 02:16:32 +0600

    Fixed rating section

--- src/Components/Ratings.jsx
+++ src/Components/Ratings.jsx
@@ -1,6 +1,6 @@
 function Ratings() {
     return (
-        <div className="w-full flex flex-row md:flex-row max-sm:flex-col items-center justify-around gap-12 max-sm:gap-4 py-12 md:py-16 px-6 max-sm:px-2 max bg-linear-to-r from-violet-600 to-indigo-600 mb-6">
+        <div className="w-full flex flex-row md:flex-row max-sm:flex-col items-center justify-center gap-8 max-sm:gap-4 py-12 md:py-16 px-6 max-sm:px-2 max bg-linear-to-r from-violet-600 to-indigo-600 mb-6">
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-5xl md:text-6xl font-bold text-white text-center">50K+</h1>
                 <p className="mt-2 text-[#DDCEFD] text-xl md:text-2xl text-center">Active Users</p>
