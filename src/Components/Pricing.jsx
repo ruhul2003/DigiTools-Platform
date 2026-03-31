11bba98ff455e5418ea80754a8e808d3a1fb21e2
ruhul2003 <ruhul941020@gmail.com>
2026-03-31 16:47:48 +0600

    Made responsive the pricing section

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -2,7 +2,7 @@ import { IoCheckmark } from "react-icons/io5";
 
 function Pricing() {
     return (
-        <div className="w-full md:w-4/5 mx-auto mt-16 mb-16 px-4">
+        <div className="w-full md:w-4/5 mx-auto mt-32 mb-16 px-4">
     {/* Header Section */}
     <h1 className="text-3xl md:text-5xl text-center font-bold">Simple, Transparent Pricing</h1>
     <p className="mt-4 text-center text-gray-600">Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
